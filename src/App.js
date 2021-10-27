215ba5037d78a09a0b411937d5f857e0915b1bc1
JeanS707 <74681689+JeanS707@users.noreply.github.com>
2021-10-27 18:04:20 -0400

    Added array hopefully it saves

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import "./styles.css";
 //Write array below
+let FirstName = [
+  "Omari",
+  "Melody",
+  "Keturah",
+  "Aldo",
+  "Reginea",
+  "Amori",
+  "Laurent",
+  "Ye",
+  "Matt",
+  "Judy",
+  "Sami",
+  "Maxwell",
+  "Groana",
+  "Aland",
+  "Jean",
+  "Lorena"
+];
+
+// let NameColor = [
+//   (31, 183, 82)
+// ];
 export default function App() {
   return (
     <div className="App">
       <table>
         <th>First Name</th>
         <tr>
-          <td>Name</td>
+          <td>{FirstName[0]}</td>
         </tr>
         <tr>
-          <td>Name</td>
+          <td>{FirstName[1]}</td>
         </tr>
         <tr>
-          <td>Name</td>
+          <td>{FirstName[2]}</td>
         </tr>
         <tr>
-          <td>Name</td>
+          <td>{FirstName[3]}</td>
         </tr>
         <tr>
-          <td>Name</td>
+          <td>{FirstName[4]}</td>
+        </tr>
+        <tr>
+          <td>{FirstName[5]}</td>
+        </tr>
+        <tr>
+          <td>{FirstName[6]}</td>
+        </tr>
+        <tr>
+          <td>{FirstName[7]}</td>
+        </tr>
+        <tr>
+          <td>{FirstName[8]}</td>
         </tr>
       </table>
     </div>
